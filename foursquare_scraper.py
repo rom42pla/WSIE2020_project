dc34cf75a3bac4f060c5060452c5a47487611e77
rom42pla <33460756+rom42pla@users.noreply.github.com>
2020-07-15 01:31:00 +0200

    Update foursquare_scraper.py

--- foursquare_scraper.py
+++ foursquare_scraper.py
@@ -13,7 +13,7 @@ from pprint import pprint
 seeds = ["201739388"]
 frontier, visited = list(copy.deepcopy(seeds)), set()
 assets_folder = "./assets"
-pois_filepath, users_filepath, frontier_filepath = f"{assets_folder}/4square_pois_file.json", "{assets_folder}/4square_users_4square.json", "{assets_folder}/4square_frontier.json"
+pois_filepath, users_filepath, frontier_filepath = f"{assets_folder}/4square_pois_file.json", f"{assets_folder}/4square_users.json", f"{assets_folder}/4square_frontier.json"
 pois, users = {}, {}
 
 if os.path.isfile(pois_filepath) and os.path.isfile(users_filepath) and os.path.isfile(frontier_filepath):
